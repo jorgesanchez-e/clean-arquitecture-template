445f0b418fae6a232d969a7b986fb2b08bfdf6f6
jorge.sanchez.escobedo@gmail.com <jorge.sanchez.escobedo@gmail.com>
2023-02-22 14:26:37 -0600

    input ports

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"context"
+
+	exampleapp "clean-arquitecture-template/internal/app/example"
+	"clean-arquitecture-template/internal/inputports/example"
+	"clean-arquitecture-template/internal/interfaceadapters/storage/mongodb"
 )
 
+var cnf config
+
 func main() {
-	fmt.Printf("Hello-world\n")
+	ctx := context.Background()
+
+	repo := mongodb.NewExampleRepo(ctx, cnf)
+	idProv := mongodb.NewIdentityProvider()
+	services := exampleapp.NewServices(repo, idProv)
+	rest := example.NewServices(ctx, services)
+
+	rest.Server.ListenAndServe("8080")
+}
+
+type config struct{}
+
+func (c config) GetDSN() string {
+	return ""
+}
+
+func (c config) DatabaseName() string {
+	return ""
+}
+
+func (c config) TableName() string {
+	return ""
 }
